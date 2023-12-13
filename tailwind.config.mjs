4af04b9bdc897fc56475c72a8ada912bfb924de6
unknown <jluisviegas@hotmail.com>
2023-12-12 21:53:24 -0300

    Footer And Some Sections Content

--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,8 +1,29 @@
 /** @type {import('tailwindcss').Config} */
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 export default {
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
 	theme: {
-		extend: {},
+		extend: {
+			fontFamily: {
+				play: ['Playfair Display Variable', ...defaultTheme.fontFamily.sans],
+				roboto: ['Roboto', ...defaultTheme.fontFamily.sans],
+			},
+			colors: {
+				primary: '#E2DDD9',
+				primaryDark: '#dbd3cf',
+				secondary: '#c16e70',
+				accent: '#757871',
+			},
+		},
+		screens: {
+			xs: '480px',
+			ss: '620px',
+			sm: '768px',
+			md: '1060px',
+			lg: '1200px',
+			xl: '1700px',
+		},
 	},
 	plugins: [],
-}
+};
