ee3b203e80dc910db12fa8b6c1ad3bdfe34f8339
unknown <jluisviegas@hotmail.com>
2024-01-11 18:27:25 -0300

    Accordion component, responsive, image optm

--- src/components/FAQ.tsx
+++ src/components/FAQ.tsx
@@ -1,88 +1,70 @@
 import { motion } from 'framer-motion';
 import { useState } from 'react';
 
-interface FAQProps {
-	title: string;
-	answer: string;
-}
-
-const questions: FAQProps[] = [
-	{
-		title: 'Terapia online funciona?',
-		answer:
-			'As pesquisas mais recentes mostram que 85% das pessoas preferem a terapia online ou a acham tão boa quanto a presencial. Segundo o Conselho Federal de Psicologia, o acompanhamento online possui a mesma eficácia do acompanhamento presencial, além de ser mais em conta e não exigir deslocamentos.	',
-	},
-	{
-		title: 'Como eu sei se devo fazer terapia?',
-		answer:
-			'A terapia pode servir para diversas funções: se conhecer melhor, cuidar da saúde emocional ou ajudar a lidar com situações de sofrimento psíquico. Algumas pessoas também optam por fazer terapia para melhorar o rendimento profissional, reduzir o estresse ou a ansiedade no dia-a-dia. Do ponto de vista clínico, a terapia passa a ser sugerida quando as suas atividades rotineiras estão sendo comprometidas por emoções negativas recorrentes e/ou sintomas físicos começam a aparecer.',
-	},
-	{
-		title:
-			'Não tenho um celular novo ou internet boa para realizar as consultas. Isso é um problema?',
-		answer:
-			'O aplicativo adapta a qualidade da chamada à qualidade do dispositivo e da conexão disponível. Você também pode optar por seguir a consulta através de chamadas de áudio, caso o vídeo não esteja sendo conveniente.',
-	},
-	{
-		title: 'Posso remarcar as minhas consultas?',
-		answer:
-			'Qualquer consulta poderá ser remarcada através do WhatsApp até uma vez, desde que a remarcação seja realizada até 24h antes do atual horário da consulta. Você poderá escolher um novo horário de acordo com a agenda disponível.',
-	},
-	{
-		title: 'Terapia online funciona?',
-		answer:
-			'As pesquisas mais recentes mostram que 85% das pessoas preferem a terapia online ou a acham tão boa quanto a presencial. Segundo o Conselho Federal de Psicologia, o acompanhamento online possui a mesma eficácia do acompanhamento presencial, além de ser mais em conta e não exigir deslocamentos.',
-	},
-];
+import {
+	Accordion,
+	AccordionContent,
+	AccordionItem,
+	AccordionTrigger,
+} from '../components/accordion';
 
 export default function FAQ() {
-	const [toggled, setToggled] = useState<number | null>(null);
-
-	const handleToggle = (index: number) => {
-		setToggled((prevIndex) => (prevIndex === index ? null : index));
-	};
-
 	return (
-		<div className="relative">
-			<img
-				src="/logo.png"
-				alt="shape"
-				width={600}
-				height={600}
-				className="absolute -top-20 left-20 opacity-10"
-			/>
-
-			{questions.map((question, index) => (
-				<div
-					key={index}
-					className="border-b-2 border-secondaryDark cursor-pointer py-6"
-				>
-					<li className=" flex justify-between item-center w-full">
-						<h3
-							className={`font-semibold text-xs sm:text-sm ${
-								toggled === index ? 'text-secondaryDark' : 'text-slate-900'
-							}`}
-						>
-							{question.title}
-						</h3>
-						<img
-							src="/down_arrow.png"
-							alt="arrow"
-							className="w-4 h-4 cursor-pointer"
-							onClick={() => handleToggle(index)}
-						/>
-					</li>
-					{toggled === index && (
-						<motion.div
-							initial={{ opacity: 0, scale: 0.8 }}
-							animate={{ opacity: 1, scale: 1 }}
-							transition={{ duration: 0.3 }}
-						>
-							<p className="pt-2 italic">{question.answer}</p>
-						</motion.div>
-					)}
-				</div>
-			))}
-		</div>
+		<Accordion type="single" collapsible className="w-full">
+			<AccordionItem value="item-1">
+				<AccordionTrigger>Terapia online funciona?</AccordionTrigger>
+				<AccordionContent>
+					As pesquisas mais recentes mostram que 85% das pessoas preferem a
+					terapia online ou a acham tão boa quanto a presencial. Segundo o
+					Conselho Federal de Psicologia, o acompanhamento online possui a mesma
+					eficácia do acompanhamento presencial, além de ser mais em conta e não
+					exigir deslocamentos.
+				</AccordionContent>
+			</AccordionItem>
+			<AccordionItem value="item-2">
+				<AccordionTrigger>Como eu sei se devo fazer terapia?</AccordionTrigger>
+				<AccordionContent>
+					A terapia pode servir para diversas funções: se conhecer melhor,
+					cuidar da saúde emocional ou ajudar a lidar com situações de
+					sofrimento psíquico. Algumas pessoas também optam por fazer terapia
+					para melhorar o rendimento profissional, reduzir o estresse ou a
+					ansiedade no dia-a-dia. Do ponto de vista clínico, a terapia passa a
+					ser sugerida quando as suas atividades rotineiras estão sendo
+					comprometidas por emoções negativas recorrentes e/ou sintomas físicos
+					começam a aparecer.
+				</AccordionContent>
+			</AccordionItem>
+			<AccordionItem value="item-3">
+				<AccordionTrigger className="text-left">
+					Não tenho um celular novo ou internet boa para realizar as consultas.
+					Isso é um problema?
+				</AccordionTrigger>
+				<AccordionContent>
+					O aplicativo adapta a qualidade da chamada à qualidade do dispositivo
+					e da conexão disponível. Você também pode optar por seguir a consulta
+					através de chamadas de áudio, caso o vídeo não esteja sendo
+					conveniente.
+				</AccordionContent>
+			</AccordionItem>
+			<AccordionItem value="item-4">
+				<AccordionTrigger>Posso remarcar as minhas consultas?</AccordionTrigger>
+				<AccordionContent>
+					Qualquer consulta poderá ser remarcada através do WhatsApp até uma
+					vez, desde que a remarcação seja realizada até 24h antes do atual
+					horário da consulta. Você poderá escolher um novo horário de acordo
+					com a agenda disponível.
+				</AccordionContent>
+			</AccordionItem>
+			<AccordionItem value="item-5">
+				<AccordionTrigger>Terapia online funciona?</AccordionTrigger>
+				<AccordionContent>
+					As pesquisas mais recentes mostram que 85% das pessoas preferem a
+					terapia online ou a acham tão boa quanto a presencial. Segundo o
+					Conselho Federal de Psicologia, o acompanhamento online possui a mesma
+					eficácia do acompanhamento presencial, além de ser mais em conta e não
+					exigir deslocamentos.
+				</AccordionContent>
+			</AccordionItem>
+		</Accordion>
 	);
 }
--- tailwind.config.mjs
+++ tailwind.config.mjs
@@ -1,15 +1,32 @@
 /** @type {import('tailwindcss').Config} */
 const defaultTheme = require('tailwindcss/defaultTheme');
 
-export default {
+module.exports = {
+	darkMode: ['class'],
 	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	prefix: '',
 	theme: {
+		container: {
+			screens: {
+				xs: '480px',
+				ss: '620px',
+				sm: '768px',
+				md: '1060px',
+				lg: '1200px',
+				xl: '1700px',
+			},
+		},
 		extend: {
 			fontFamily: {
 				play: ['Playfair Display Variable', ...defaultTheme.fontFamily.sans],
 				mont: ['Montserrat Variable', ...defaultTheme.fontFamily.sans],
 			},
 			colors: {
+				border: 'hsl(var(--border))',
+				input: 'hsl(var(--input))',
+				ring: 'hsl(var(--ring))',
+				background: 'hsl(var(--background))',
+				foreground: 'hsl(var(--foreground))',
 				primary: '#E2DDD9',
 				primaryDark: '#dbd3cf',
 				secondary: '#c16e70',
@@ -17,16 +34,47 @@ export default {
 				lightBlue: '#BFCDD9',
 				darkBlue: '#475569',
 				purpleDark: '#3b344f',
+				destructive: {
+					DEFAULT: 'hsl(var(--destructive))',
+					foreground: 'hsl(var(--destructive-foreground))',
+				},
+				muted: {
+					DEFAULT: 'hsl(var(--muted))',
+					foreground: 'hsl(var(--muted-foreground))',
+				},
+				accent: {
+					DEFAULT: 'hsl(var(--accent))',
+					foreground: 'hsl(var(--accent-foreground))',
+				},
+				popover: {
+					DEFAULT: 'hsl(var(--popover))',
+					foreground: 'hsl(var(--popover-foreground))',
+				},
+				card: {
+					DEFAULT: 'hsl(var(--card))',
+					foreground: 'hsl(var(--card-foreground))',
+				},
+			},
+			borderRadius: {
+				lg: 'var(--radius)',
+				md: 'calc(var(--radius) - 2px)',
+				sm: 'calc(var(--radius) - 4px)',
+			},
+			keyframes: {
+				'accordion-down': {
+					from: { height: '0' },
+					to: { height: 'var(--radix-accordion-content-height)' },
+				},
+				'accordion-up': {
+					from: { height: 'var(--radix-accordion-content-height)' },
+					to: { height: '0' },
+				},
+			},
+			animation: {
+				'accordion-down': 'accordion-down 0.2s ease-out',
+				'accordion-up': 'accordion-up 0.2s ease-out',
 			},
 		},
-		screens: {
-			xs: '480px',
-			ss: '620px',
-			sm: '768px',
-			md: '1060px',
-			lg: '1200px',
-			xl: '1700px',
-		},
 	},
-	plugins: [],
+	plugins: [require('tailwindcss-animate')],
 };
